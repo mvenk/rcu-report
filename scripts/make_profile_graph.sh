3016e9f9f5e0f0cb5ef4a04d39fd4397fa8dbc57
Madhuri Venkatesh <madhureev@gmail.com>
2012-01-17 20:37:31 -0800

    generating graphs

--- scripts/make_profile_graph.sh
+++ scripts/make_profile_graph.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+imagedir="../images/graphs"
 ./make_profile_dat.sh 8 rwl v 1 "Master::check_driver" "click.ReadWriteLockUser::acquire_read" "RouterThread::lock_tasks" > profile1.dat
-./bargraph.pl profile1.dat > ../images/profile_macro_rwl_vr_1w.eps
+./bargraph.pl profile1.dat > ${imagedir}/profile_macro_rwl_vr_1w.eps
 ./make_profile_dat.sh 8 rcu v 1 "Master::check_driver" "ReadRadixIPLookupM106::run_task" "RouterThread::lock_tasks"> profile2.dat
-./bargraph.pl profile2.dat > ../images/profile_macro_rcu_vr_1w.eps
+./bargraph.pl profile2.dat > ${imagedir}/profile_macro_rcu_vr_1w.eps
 
--- scripts/makegraph.sh
+++ scripts/makegraph.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-rcubranch="$HOME/click_rcu/race/macrobenchmarks/out"
-rwlbranch="$HOME/click_rcu_master_tests/race/macrobenchmarks/out"
 imgdir="../images/graphs"
+array="macro micro"
 
+for type in $array;
+do
+rcubranch="$HOME/click_rcu/race/${type}benchmarks/out"
+rwlbranch="$HOME/click_rcu_master_tests/race/${type}benchmarks/out"
 # graph.sh takes  care of graphs with pure reader workload- takes 3 inputs one for RCU, RWL and no locks
 #./graph.sh  $rcubranch/testsummary_micro_rcu_vr_0w.out $rwlbranch/testsummary_micro_rwl_vr_0w.out $rwlbranch/testsummary_micro_non_vr_0w.out $imgdir/micro_vr_0w.eps
-./graph.sh  $rcubranch/testsummary_macro_rcu_vr_0w.out $rwlbranch/testsummary_macro_rwl_vr_0w.out $rwlbranch/testsummary_macro_non_vr_0w.out $imgdir/macro_vr_0w.eps
+./graph.sh  $rcubranch/testsummary_${type}_rcu_vr_0w.out $rwlbranch/testsummary_${type}_rwl_vr_0w.out $rwlbranch/testsummary_${type}_non_vr_0w.out $imgdir/${type}_vr_0w.eps
 # graphwriter.sh is for graphs with at least one writer - takes 2 input files one for RCU and RWL.
-./graphwriter.sh  $rcubranch/testsummary_macro_rcu_vr_1w.out $rwlbranch/testsummary_macro_rwl_vr_1w.out $imgdir/macro_vr_1w.eps
-#./graphwriter.sh  $rcubranch/testsummary_micro_rcu_vr_1w.out $rwlbranch/testsummary_micro_rwl_vr_1w.out ../images/micro_vr_1w.eps
-./graphwriter.sh  $rcubranch/testsummary_macro_rcu_1r_vw.out $rwlbranch/testsummary_macro_rwl_1r_vw.out $imgdir/macro_1r_vw.eps
-#./graphwriter.sh  $rcubranch/testsummary_micro_rcu_1r_vw.out $rwlbranch/testsummary_micro_rwl_1r_vw.out ../images/micro_1r_vw.eps
-#./graphwriter.sh  $rcubranch/testsummary_macro_rcu_0r_vw.out $rwlbranch/testsummary_macro_rwl_0r_vw.out ../images/macro_0r_vw.eps
-#./graphwriter.sh  $rcubranch/testsummary_macro_rcu_0r_vw.out $rwlbranch/testsummary_micro_rwl_0r_vw.out ../images/micro_0r_vw.eps
+./graphwriter.sh  $rcubranch/testsummary_${type}_rcu_vr_1w.out $rwlbranch/testsummary_${type}_rwl_vr_1w.out $imgdir/${type}_vr_1w.eps
+./graphwriter.sh  $rcubranch/testsummary_${type}_rcu_1r_vw.out $rwlbranch/testsummary_${type}_rwl_1r_vw.out $imgdir/${type}_1r_vw.eps
+./graphwriter.sh  $rcubranch/testsummary_${type}_rcu_0r_vw.out $rwlbranch/testsummary_${type}_rwl_0r_vw.out ../images/$imgdir/${type}_0r_vw.eps
+done
 
 #maintain a png version of each file for email
 #elements=`ls -1 $imgdir *.eps | cut -d. -f1 | sed -e 's/\.click//g'`
